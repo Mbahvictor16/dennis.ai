4f611555771b9f62b8306298af6c3278ce62bde5
Mbahvictor16 <72951604+Mbahvictor16@users.noreply.github.com>
2023-05-01 22:24:13 +0100

    first commit

---
+++ client/public/script/chat.js
@@ -0,0 +1,95 @@
+const messageDiv = document.querySelector(".message");
+const formBox = document.querySelector("form");
+const textBox = document.querySelector("textarea");
+const botIcon = `<i class="fas fa-robot"></i>`;
+const userIcon = '<i class="fa fa-user" aria-hidden="true"></i>';
+let loadInterval;
+
+function loader(element) {
+  loadInterval = setInterval(() => {
+    element.innerHTML += "|";
+    if (element.innerHTML === "||") {
+      element.innerHTML = "";
+    }
+  }, 1000);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 30);
+}
+
+function generateId() {
+  return Date.now();
+}
+
+function chatContainer(isAi, value, uniqueId) {
+  return `
+    <div class=" wrapper ${isAi ? "bot" : "human"}">
+      <div class="img">
+        ${isAi ? botIcon : userIcon}
+      </div>
+      <div class="text" id="${uniqueId}">${value}</div>
+    </div>
+  `;
+}
+
+async function sendText(e) {
+  e.preventDefault();
+
+  // user stripe
+  if (!textBox.value || textBox.value === "" || textBox.value === null) {
+    e.preventDefault();
+    return;
+  }
+  messageDiv.innerHTML += chatContainer(false, textBox.value);
+
+  // bot stripe
+  let uniqueId = generateId();
+  messageDiv.innerHTML += chatContainer(true, "", uniqueId);
+  textBox.readOnly = true;
+  messageDiv.scrollTop = messageDiv.scrollHeight;
+  const responseMessage = document.getElementById(uniqueId);
+  loader(responseMessage);
+
+  // bot response
+  const response = await fetch("http://localhost:5000/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: textBox.value,
+    }),
+  });
+
+  formBox.reset();
+
+  clearInterval(loadInterval);
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot_response.trim();
+
+    typeText(responseMessage, parsedData);
+  } else {
+  }
+
+  textBox.readOnly = false;
+}
+
+formBox.addEventListener("submit", sendText);
+
+formBox.addEventListener("keydown", (e) => {
+  if (e.key === "Alt" && e.keyCode === 13) {
+    sendText(e);
+  }
+});
---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const configuration = new Configuration({
+  organization: "org-egUd1Ev1Pl5y872SQcVw8GM8",
+  apiKey: process.env.OPEN_AI,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from DennisAi",
+  });
+});
+
+app.get("/chat", async (req, res) => {
+  res.status(200).send({
+    message: "Ask DennisAi",
+  });
+});
+
+app.post("/chat", async (req, res) => {
+  try {
+    const result = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${req.body.prompt}`,
+      temperature: 0,
+      max_tokens: 5000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot_response: result.data.choices[0].text,
+    });
+  } catch (error) {
+    res.status(500).send({
+      err_message: error,
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("The server is running");
+});
